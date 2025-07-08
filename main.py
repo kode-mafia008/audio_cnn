57a8d529059fd9ef290c618fe75a755ec84526de
dharmendra008 <dharmendramehta622@gmail.com>
2025-07-08 13:34:39 +0545

    feat: update audio sample rate to 44100Hz and add feature map visualization support

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import base64
+from librosa.core import audio
 import modal 
 import torch.nn as nn
 import torchaudio.transforms as T
 import torch
 import io
+import numpy as np
 from pydantic import BaseModel
 import soundfile as sf
 import librosa
@@ -26,7 +28,7 @@ class AudioProcessor:
     def __init__(self):
         self.transform = nn.Sequential(
             T.MelSpectrogram(
-                sample_rate=22050,
+                sample_rate=44100,
                 n_fft=1024,
                 hop_length=512,
                 n_mels = 128,
@@ -78,25 +80,61 @@ class AudioClassifier:
         if audio_data.ndim > 1:
             audio_data = np.mean(audio_data,axis=1)
         
-        if sample_rate != 22050:
-            audio_data = librosa.resample(audio_data,sample_rate,target_sr=22050)
+        if sample_rate != 44100:
+            audio_data = librosa.resample(audio_data,sample_rate,target_sr=44100)
 
         spectogram = self.audio_processor.process_audio_chunk(audio_data)
         spectogram = spectogram.to(self.device)
 
         with torch.no_grad():
-            output = self.model(spectogram)
+            output,feature_maps = self.model(spectogram,return_feature_maps=True)
             output = torch.nan_to_num(output)
             probabilities = torch.softmax(output,dim=1) # dim=0 batch, dim=1 classes
             top3_probs, top3_indices = torch.topk(probabilities[0],k=3,dim=0)
 
             # top3_probs: [0.8,0.1,0.1], top3_indices: [0,1,2]
             predictions = [{"class": self.classes[idx.item()], "confidence":prob.item()} for prob,idx in zip(top3_probs,top3_indices)]
-        
-        response = {
-            "predictions": predictions
-        }
-        return response
+            viz_data = {}
+            for name,tensor in feature_maps.items():
+                if tensor.dim() == 4: # [batch_size,channels,height,width]
+                    aggregated_tensor = torch.mean(tensor,dim=1)
+                    squeezed_tensor = aggregated_tensor.squeeze(0)
+                    numpy_array = squeezed_tensor.cpu().numpy()
+                    clean_array = np.nan_to_num(numpy_array)
+                    viz_data[name] = {
+                        "shape":clean_array.shape,
+                        "values":clean_array.tolist()
+                    }
+            
+            # [batch,channel,height,width]
+            spectogram_np = spectogram.squeeze(0).squeeze(0).cpu().numpy() # squeeze the batch and channel only left with [height,width]
+            clean_spectogram = np.nan_to_num(spectogram_np)
+
+            max_samples = 8000
+            if len(audio_data) > max_samples:
+                step = len(audio_data) // max_samples
+                # This line takes every nth sample from the audio data to reduce its size.
+                # For example, if the audio data has 16000 samples and max_samples is 8000,
+                # it will take every 2nd sample, resulting in 8000 samples.
+                # e.g. [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] -> [0, 2, 4, 6, 8]
+                waveform_data = audio_data[::step]
+            else:
+                waveform_data = audio_data
+
+            response = {
+                "predictions": predictions,
+                "visualization": viz_data,
+                "input_spectogram":{
+                    "shape":clean_spectogram.shape,
+                    "values":clean_spectogram.tolist()
+                },
+                "waveform":{
+                    "values": waveform_data.tolist(),
+                    "sample_rate": 44100,
+                    "duration": len(audio_data) / 44100
+                } 
+            }
+            return response
 
 
 @app.local_entrypoint()
@@ -104,7 +142,7 @@ def main():
     audio_data,sample_rate = sf.read("chirpingbirds.wav")
 
     buffer = io.BytesIO()
-    sf.write(buffer,audio_data,22050,format="WAV")
+    sf.write(buffer,audio_data,44100,format="WAV")
     audio_b64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
     payload = {"audio_data":audio_b64}
 
@@ -114,7 +152,12 @@ def main():
     response = requests.post(url,json=payload)
 
     result = response.json()
-
+    
+    waveform_info = result.get('waveform',{})
+    waveform_data = waveform_info.get('values',[])
+    print(f"First 10 values: {[round(v,4) for v in waveform_data[:10]]}")
+    print(f"Duration: {waveform_info.get('duration',0)}")
+    
     print("Top predictions: ")
 
     for pred in result.get('predictions',[]):
--- model.py
+++ model.py
@@ -18,7 +18,7 @@ class ResidualBlock(nn.Module):
                 nn.BatchNorm2d(out_channels)
             )
     
-    def forward(self,x):
+    def forward(self,x,fmap_dict=None,prefix=""):
         out = self.conv1(x)
         out = self.bn1(out)
         out = torch.relu(out)
@@ -29,7 +29,14 @@ class ResidualBlock(nn.Module):
         shortcut = self.shortcut(x) if self.use_shortcut else x 
 
         out_add = out + shortcut
+
+        if fmap_dict is not None:
+            fmap_dict[f'{prefix}.conv'] = out_add
+
         out = torch.relu(out_add)
+
+        if fmap_dict is not None:
+            fmap_dict[f'{prefix}.relu'] = out
 
         return out
         
@@ -56,21 +63,49 @@ class AudioCNN(nn.Module):
         self.dropout = nn.Dropout(0.5)
         self.fc = nn.Linear(in_features=512,out_features=num_classes)
     
-    def forward(self,x):
-        x = self.conv1(x)
+    def forward(self,x,return_feature_maps=False):
+        if not return_feature_maps:
+            x = self.conv1(x)
 
-        for block in self.layer1:
-            x = block(x)
-        for block in self.layer2:
-            x = block(x)
-        for block in self.layer3:
-            x = block(x)
-        for block in self.layer4:
-            x = block(x)
-        
-        x = self.avgpool(x)
-        x = x.view(x.size(0),-1)
-        x = self.dropout(x)
-        x = self.fc(x)
-        return x 
-        
+            for block in self.layer1:
+                x = block(x)
+            for block in self.layer2:
+                x = block(x)
+                for block in self.layer3:
+                    x = block(x)
+                for block in self.layer4:
+                    x = block(x)
+                
+                x = self.avgpool(x)
+                x = x.view(x.size(0),-1)
+                x = self.dropout(x)
+                x = self.fc(x)
+
+                return x 
+        else:
+            feature_maps = {}
+            x = self.conv1(x)
+            feature_maps['conv1'] = x
+
+            for i,block in enumerate(self.layer1):
+                x= block(x,fmap_dict=feature_maps,prefix=f"layer1.block{i}")
+            feature_maps['layer1'] = x
+
+            for i,block in enumerate(self.layer2):
+                x = block(x,fmap_dict=feature_maps,prefix=f"layer2.block{i}")
+            feature_maps['layer2'] = x
+
+            for i,block in enumerate(self.layer3):
+                x = block(x,fmap_dict=feature_maps,prefix=f"layer3.block{i}")
+            feature_maps['layer3'] = x
+
+            for i,block in enumerate(self.layer4):
+                x = block(x,fmap_dict=feature_maps,prefix=f"layer4.block{i}")
+            feature_maps['layer4'] = x
+
+            x = self.avgpool(x)
+            x = x.view(x.size(0),-1)
+            x = self.dropout(x)
+            x = self.fc(x)
+
+            return x,feature_maps 
